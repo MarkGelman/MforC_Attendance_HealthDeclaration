303978168d9d828686fa9ac693631a885c662b5e
Mark <markgora1974@gmail.com>
2021-03-10 12:52:14 +0200

    Add project files.

---
+++ MforC_Attendance_HealthDeclaration/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace MforC_Attendance_HealthDeclaration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdbAttendanceFull = new System.Windows.Forms.RadioButton();
+            this.rdbAttendance = new System.Windows.Forms.RadioButton();
+            this.rdbNoAttendance = new System.Windows.Forms.RadioButton();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.dgvA_HD = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtRemark = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvA_HD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdbAttendanceFull
+            // 
+            this.rdbAttendanceFull.AutoSize = true;
+            this.rdbAttendanceFull.Location = new System.Drawing.Point(700, 12);
+            this.rdbAttendanceFull.Name = "rdbAttendanceFull";
+            this.rdbAttendanceFull.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.rdbAttendanceFull.Size = new System.Drawing.Size(84, 17);
+            this.rdbAttendanceFull.TabIndex = 0;
+            this.rdbAttendanceFull.TabStop = true;
+            this.rdbAttendanceFull.Text = "נוכח והביא";
+            this.rdbAttendanceFull.UseVisualStyleBackColor = true;
+            this.rdbAttendanceFull.CheckedChanged += new System.EventHandler(this.rdbAttendanceFull_CheckedChanged);
+            // 
+            // rdbAttendance
+            // 
+            this.rdbAttendance.AutoSize = true;
+            this.rdbAttendance.Location = new System.Drawing.Point(682, 51);
+            this.rdbAttendance.Name = "rdbAttendance";
+            this.rdbAttendance.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.rdbAttendance.Size = new System.Drawing.Size(102, 17);
+            this.rdbAttendance.TabIndex = 1;
+            this.rdbAttendance.TabStop = true;
+            this.rdbAttendance.Text = "נוכח ולא הביא";
+            this.rdbAttendance.UseVisualStyleBackColor = true;
+            this.rdbAttendance.CheckedChanged += new System.EventHandler(this.rdbAttendance_CheckedChanged);
+            // 
+            // rdbNoAttendance
+            // 
+            this.rdbNoAttendance.AutoSize = true;
+            this.rdbNoAttendance.Location = new System.Drawing.Point(717, 88);
+            this.rdbNoAttendance.Name = "rdbNoAttendance";
+            this.rdbNoAttendance.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.rdbNoAttendance.Size = new System.Drawing.Size(67, 17);
+            this.rdbNoAttendance.TabIndex = 2;
+            this.rdbNoAttendance.TabStop = true;
+            this.rdbNoAttendance.Text = "לא נוכח";
+            this.rdbNoAttendance.UseVisualStyleBackColor = true;
+            this.rdbNoAttendance.CheckedChanged += new System.EventHandler(this.rdbNoAttendance_CheckedChanged);
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(291, 5);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dgvStudents.Size = new System.Drawing.Size(350, 447);
+            this.dgvStudents.TabIndex = 3;
+            this.dgvStudents.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudents_CellContentClick);
+            // 
+            // dgvA_HD
+            // 
+            this.dgvA_HD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvA_HD.Location = new System.Drawing.Point(13, 5);
+            this.dgvA_HD.Name = "dgvA_HD";
+            this.dgvA_HD.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dgvA_HD.Size = new System.Drawing.Size(272, 447);
+            this.dgvA_HD.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(664, 134);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(124, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // txtRemark
+            // 
+            this.txtRemark.Location = new System.Drawing.Point(682, 180);
+            this.txtRemark.Name = "txtRemark";
+            this.txtRemark.Size = new System.Drawing.Size(100, 20);
+            this.txtRemark.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtRemark);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dgvA_HD);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.rdbNoAttendance);
+            this.Controls.Add(this.rdbAttendance);
+            this.Controls.Add(this.rdbAttendanceFull);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvA_HD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbAttendanceFull;
+        private System.Windows.Forms.RadioButton rdbAttendance;
+        private System.Windows.Forms.RadioButton rdbNoAttendance;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.DataGridView dgvA_HD;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtRemark;
+    }
+}
+
---
+++ MforC_Attendance_HealthDeclaration/Form1.cs
@@ -0,0 +1,187 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MforC_Attendance_HealthDeclaration
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlDataAdapter adapter;
+        string conn_string = "Server =localhost;Port=5432;Database=DB_Cohar;User Id = postgres;Password=admin";
+        NpgsqlConnection conn;
+        long student_id;
+        bool flagSelect = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (TestDBConnection(conn_string))
+            {
+                try
+                {
+                    
+
+                    using (conn = new NpgsqlConnection(conn_string))
+                    {
+                        conn.Open();
+
+                        //NpgsqlCommand command = new NpgsqlCommand("sp_students_for_attendance_healthDeclaration()", conn);
+                        //command.CommandType = System.Data.CommandType.StoredProcedure; // this is default
+                        adapter = new NpgsqlDataAdapter("SELECT id,first_name,last_name,teacher FROM students", conn);
+                        DataTable tb = new DataTable();
+                        adapter.Fill(tb);
+                        dgvStudents.DataSource = tb;
+                        //adapter = new NpgsqlDataAdapter($"SELECT student_id,day,attendance,health_declaration,remark from attendance_declaration ad Join students s on ad.student_id = s.id WHERE ad.student_id = (select id from students limit 1)",conn);
+
+
+                        //используя AddRange мы можем сразу передать СП полученый массив параметров
+                        //command.Parameters.AddRange(parameters);
+
+                       /* var reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Dictionary<string, object> one_row = new Dictionary<string, object>();
+                            foreach (var item in reader.GetColumnSchema())
+                            {
+                                object column_value = reader[item.ColumnName];
+                                one_row.Add(item.ColumnName, column_value);
+                                switch (item.ColumnName)
+                                {
+                                    case "_id": cmbId.Items.Add(column_value.ToString()); break;
+                                    case "_first_name": cmbName.Items.Add(column_value.ToString()); break;
+                                    case "_last_name": cmbLastName.Items.Add(column_value.ToString()); break;
+                                }
+                            }
+                            items.Add(one_row);
+                        }*/
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex}");
+                    //Console.WriteLine($"Function {sp_name} failed. parameters: {string.Join(",", parameters.Select(_ => _.ParameterName + " : " + _.Value))}");
+                }
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Connection failed!!!");
+                Console.ReadKey();
+            }
+
+        }
+
+        bool TestDBConnection(string conn_string)
+        {
+            try
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(conn_string))
+                {
+                    conn.Open();
+                }
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void rdbAttendanceFull_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdbAttendanceFull.Checked )
+            {
+                if (flagSelect)
+                {
+                    using (conn = new NpgsqlConnection(conn_string))
+                    {
+                        conn.Open();
+                        string picker = dateTimePicker1.Value.ToShortDateString().ToString();
+                        adapter = new NpgsqlDataAdapter($"Insert INTO attendance_declaration( student_id,day,attendance,health_declaration,remark) VALUES({student_id}, {picker}, true, true,{txtRemark.Text})", conn);
+                        UpdateTableInDataGrid();
+                        flagSelect = false;
+                    }
+                }
+                   
+                rdbAttendanceFull.Checked = false;
+            }
+
+        }
+
+        private void rdbAttendance_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbAttendance.Checked)
+            {
+                if (flagSelect)
+                {
+                    using (conn = new NpgsqlConnection(conn_string))
+                    {
+                        conn.Open();
+                        adapter = new NpgsqlDataAdapter($"Insert INTO attendance_declaration VALUES({student_id}, {dateTimePicker1.Value.ToShortDateString()}, true, false,{txtRemark.Text})", conn);
+                        UpdateTableInDataGrid();
+                        flagSelect = false;
+                    }
+                } 
+                else
+                    rdbAttendanceFull.Checked = false;
+            }
+        }
+
+        private void rdbNoAttendance_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (flagSelect)
+            {
+                using (conn = new NpgsqlConnection(conn_string))
+                {
+                    conn.Open();
+                    adapter = new NpgsqlDataAdapter($"Insert INTO attendance_declaration VALUES({student_id}, {dateTimePicker1.Value.ToShortDateString()}, false, false,{txtRemark.Text})", conn);
+                    UpdateTableInDataGrid();
+                    flagSelect = false;
+                }
+            }
+                
+            else
+                rdbAttendanceFull.Checked = false;
+        }
+        private void dgvStudents_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rdbAttendance.Checked = false;
+            rdbAttendanceFull.Checked = false;
+            rdbNoAttendance.Checked = false;
+            flagSelect = true;
+            student_id = (long) dgvStudents.Rows[e.RowIndex].Cells["id"].Value;
+        }
+
+        private void UpdateTableInDataGrid()
+        {
+            using (conn = new NpgsqlConnection(conn_string))
+            {
+                conn.Open();
+                string picker = dateTimePicker1.Value.ToShortDateString().ToString();
+                adapter = new NpgsqlDataAdapter($"SELECT id,day,attendance,health_declaration,remark from attendance_declaration WHERE day = '10/03/21'", conn);
+                DataTable tb = new DataTable();
+                adapter.Fill(tb);
+                dgvA_HD.DataSource = tb;
+            }
+            
+        }
+        
+    }
+    
+}
